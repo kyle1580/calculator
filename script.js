5ce42cc61141b77929ca1eb1f7953b5be4198e56
Kyle <kylecindric@gmail.com>
2022-02-02 17:21:07 -0600

    Finished

--- script.js
+++ script.js
@@ -33,4 +33,53 @@ function operate(operator , num1 , num2) {
     }
 }
 
+const display = document.querySelector('.display');
+let displayValue = [];
+let operator;
+let num1;
+let num2;
+let sum;
+
+function removeTransition() {
+    buttons.forEach(button => {
+        if(button.classList.contains('clicked')) {
+            button.removeEventListener('transitionend',removeTransition)
+            button.classList.toggle('clicked');
+        }
+    })
+}
+
+function addToDisplay(id , displayValue) {
+    displayValue.push(id);
+    display.textContent = displayValue.join('');
+}
+
 const buttons = Array.from(document.querySelectorAll(".button"));
+buttons.forEach(button => {
+    button.addEventListener('click' , () => {
+        button.classList.toggle('clicked');
+        if(button.classList.contains('number')) {
+            addToDisplay(button.id , displayValue)
+        }
+        if(button.id === 'clear') {
+            display.textContent = '';
+            displayValue = [];
+            sum = undefined;
+        }
+        if(button.classList.contains('function')) {
+            operator = button.id;
+            num1 = Number(display.textContent);
+            displayValue = [];
+            display.textContent = '';
+        }
+        if(button.id === '=') {
+            num2 = Number(display.textContent);
+            displayValue = [];
+            sum = operate(operator , num1 , num2);
+            display.textContent = sum;
+            num1 = sum;
+            num2 = 0;
+        }
+        button.addEventListener('transitionend' , removeTransition);
+    })
+})
